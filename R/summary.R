1cd7057143635b07ce0623b2c4eee4a94da475ed
m-kolomanski <m.kolomanski@yahoo.com>
2025-07-22 19:51:28 +0200

    feat: added function for summarizing number of planets for each star

---
+++ R/summary.R
@@ -0,0 +1,23 @@
+#' Summarize star occurances
+#' 
+#' @details 
+#' The function takes in KOI data frame and summarizes the number of planets that appear in a
+#' given dataset for each star.
+#' 
+#' @param data A data frame with KOI data. Must contain `kepoi_name` column.
+#' @returns A data farme containing:
+#'           - `Star` column with star ID.
+#'           - `Count` column with number of planets.
+#' 
+#' @importFrom dplyr `%>%` mutate group_by n
+#' @export
+summarize_star_occurances = function(data) {
+  data %>%
+    mutate(
+      Star = sapply(kepoi_name, \(x) unlist(strsplit(x, "\\."))[[1]])
+    ) %>%
+    group_by(Star) %>%
+    summarise(
+      Count = n()
+    )
+}
