eb082de0da656a7b1283d240be4e1645f13b8b40
JKolomanski <kubula@onet.eu>
2025-07-27 13:27:21 +0200

    refactor: calculate_stellar_flux  now takes in singular values instead of data frame

--- R/calculate_stellar_flux.R
+++ R/calculate_stellar_flux.R
@@ -1,51 +1,48 @@
 #' Calculate stellar flux value
 #'
 #' @details
-#' This function calculates the stellar flux for each entry in the input data frame.
+#' This function calculates the stellar flux based on provided values.
 #' It assumes luminosity is either logarithmic (log10 of L/Lsun) or linear, and optionally
 #' converts flux to absolute units (W/m²) if requested.
 #'
 #' Stellar flux is the amount of energy from a star that reaches a given area per unit time.
 #'
-#' @param data A data frame containing `objectid`, `st_lum`, and `pl_orbsmax`.
-#' @param log_lum Logical. If TRUE, assumes `st_lum` is in log10(L / L☉). Defaults to TRUE.
+#' @param st_lum Numeric. Stellar luminosity (log10(L/Lsun) or linear).
+#' @param pl_orbsmax Numeric. Orbital distance in AU.
+#' @param pl_insol Optional numeric. Precomputed flux value. If provided, it is returned as-is.
+#' @param log_lum Logical. If TRUE, assumes `st_lum` is in log10(L / Lsun). Defaults to TRUE.
 #' @param unit Character. Either `"relative"` (default) or `"wm2"` to convert to W/m².
 #'
-#' @returns A data frame with columns `objectid` and `pl_insol`.
+#' @returns Numeric. Stellar flux (relative or in W/m²).
 #'
-#' @importFrom dplyr `%>%` coalesce
 #' @export
-calculate_stellar_flux = function(data, log = TRUE, unit = "relative") {
-  if (!"data.frame" %in% class(data)) {
-    stop("Data must be a `data.frame`.")
+calculate_stellar_flux = function(st_lum, pl_orbsmax,
+                                  pl_insol = NA,
+                                  log_lum = TRUE,
+                                  unit = "relative") {
+  # Return existing value if present.
+  # We do it to not override a pl_insol value
+  # if already present when applying this function to a whole dataset.
+  if (!is.na(pl_insol)) {
+    return(pl_insol)
   }
 
-  if (nrow(data) == 0) {
-    warning("Empty data.")
-    return(NULL)
+  if (is.na(st_lum) || is.na(pl_orbsmax)) {
+    missing = c()
+    if (is.na(st_lum)) missing = c(missing, "st_lum")
+    if (is.na(pl_orbsmax)) missing = c(missing, "pl_orbsmax")
+
+    warning("Missing arguments: ", paste(missing, collapse = ", "))
+    return(NA)
   }
 
-  required_cols = c("objectid", "st_lum", "pl_orbsmax")
-  missing_cols = setdiff(required_cols, colnames(data))
-  if (length(missing_cols) > 0) {
-    stop("Invalid data provided. Missing columns: ", paste(missing_cols, collapse = ", "))
-  }
-
-  if (log) {
-    sf = 10^data$st_lum / data$pl_orbsmax^2
-  } else {
-    sf = data$st_lum / data$pl_orbsmax^2
+  if (log_lum) {
+    st_lum = 10 ^ st_lum
   }
 
   if (unit == "wm2") {
-    solar_const = 1361
-    sf = sf * solar_const
+    (st_lum / pl_orbsmax^2) * 1361
+  } else {
+    st_lum / pl_orbsmax^2
   }
-
-  data = data %>%
-    mutate(
-      pl_insol = coalesce(pl_insol, sf)
-    )
-
-  data %>% select(objectid, pl_insol)
 }
