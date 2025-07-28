0e23455b927f1ceb418753c89b4bd9045f2fb16e
JKolomanski <kubula@onet.eu>
2025-07-28 15:10:43 +0200

    feat: added function to classify planet type

---
+++ tests/testthat/test-classify_planet_type.R
@@ -0,0 +1,39 @@
+describe("classify_planet_type", {
+  it("outputs correct planet code", {
+    test_bmasse = 1
+    test_eqt = 392
+    test_orbeccen = 0.0112
+    test_dens = 5.51
+
+    expected_code = "EW0t"
+
+    expect_equal(classify_planet_type(test_bmasse,
+                                      test_eqt,
+                                      test_orbeccen,
+                                      test_dens), expected_code)
+  })
+
+  it("throws an error if one of the arguments is negative", {
+    test_bmasse = -1
+    test_eqt = 392
+    test_orbeccen = 0.0112
+    test_dens = 5.51
+
+    expect_error(
+      classify_planet_type(test_bmasse, test_eqt, test_orbeccen, test_dens),
+      "Invalid data type. `pl_bmasse` must be `numeric` and larger than 0."
+    )
+  })
+
+  it("throws an error if one of the arguments is the wrong type", {
+    test_bmasse = ""
+    test_eqt = 392
+    test_orbeccen = 0.0112
+    test_dens = 5.51
+
+    expect_error(
+      classify_planet_type(test_bmasse, test_eqt, test_orbeccen, test_dens),
+      "Invalid data type. `pl_bmasse` must be `numeric` and larger than 0."
+    )
+  })
+})
