78db5bbcc9c757cc2ac6810e25b5679517667c84
JKolomanski <kubula@onet.eu>
2025-07-28 15:30:08 +0200

    fix: fixed incorrect indentation in test-trim_ps_table

--- tests/testthat/test-trim_ps_table.R
+++ tests/testthat/test-trim_ps_table.R
@@ -1,7 +1,7 @@
 describe("trim_ps_table", {
   it("returns data frame with exactly 20 columns", {
-trimmed_data = trim_ps_table(closest_50_exoplanets)
-expect_equal(ncol(trimmed_data), 20)
+    trimmed_data = trim_ps_table(closest_50_exoplanets)
+    expect_equal(ncol(trimmed_data), 20)
   })
 
   it("throws an error if required columns are missing", {
