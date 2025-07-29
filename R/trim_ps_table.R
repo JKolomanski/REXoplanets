30c46968d25ecff83fc1885ef22091baa8bb240f
JKolomanski <kubula@onet.eu>
2025-07-29 21:38:27 +0200

    fix: removed redundant comment in docstring for trim_ps_table

--- R/trim_ps_table.R
+++ R/trim_ps_table.R
@@ -31,7 +31,8 @@
 #'
 #' @importFrom dplyr select all_of
 #'
-#' @examples trim_ps_table(ps_data) # Trim data from planetary systems table
+#' @examples
+#' trim_ps_table(ps_data)
 #'
 #' @export
 trim_ps_table = function(data) {
