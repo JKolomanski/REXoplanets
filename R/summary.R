7c3895bfa3bb7b3b42d5789f238aaad8cf015982
m-kolomanski <m.kolomanski@yahoo.com>
2025-07-31 18:20:12 +0200

    Removed ADQL, bmasse and teff from WORDLIST

--- R/summary.R
+++ R/summary.R
@@ -1,17 +1,17 @@
-#' Summarize star occurances
+#' Summarize star occurrences
 #'
 #' @details
 #' The function takes in KOI data frame and summarizes the number of planets that appear in a
 #' given dataset for each star.
 #'
 #' @param data A data frame with KOI data. Must contain `kepoi_name` column.
-#' @returns A data farme containing:
+#' @returns A data frame containing:
 #'           - `Star` column with star ID.
 #'           - `Count` column with number of planets.
 #'
 #' @importFrom dplyr `%>%` mutate group_by n summarise
 #' @export
-summarize_star_occurances = function(data) {
+summarize_star_occurrences = function(data) {
   if (!"data.frame" %in% class(data)) {
     stop("Exoplanets data must be a `data.frame`")
   }
