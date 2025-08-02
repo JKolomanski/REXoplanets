d082e54406875a8a98f8c76f90efcec5bb82aaa5
m-kolomanski <m.kolomanski@yahoo.com>
2025-08-02 10:48:44 +0200

    style: lintr, spellcheck

--- R/module_test.R
+++ R/module_test.R
@@ -1,13 +1,13 @@
 #' Test module
 #' @name module_test
 #' @details
-#' This is just a test module for reference for further developement.
+#' This is just a test module for reference for further development.
 NULL
 
 #' @param id A unique identifier for the module.
-#' @returns A Shiny UI object.
+#' @returns A Shiny `UI` object.
 #' @importFrom shiny NS div
-#' @describeIn module_test UI function for the module.
+#' @describeIn module_test `UI` function for the module.
 #' @export
 test_ui = function(id) {
   ns = NS(id)
