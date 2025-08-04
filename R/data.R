650c0ce3673faddf63b1ddfaca1c881eefef15a9
JKolomanski <kubula@onet.eu>
2025-08-04 19:30:36 +0200

    feat: added variables with column labels

--- R/data.R
+++ R/data.R
@@ -8,3 +8,19 @@
 #' @format A data frame with 683 columns and 50 rows.
 #' @source \url{https://exoplanetarchive.ipac.caltech.edu/}
 "closest_50_exoplanets"
+
+#' ps_colnames
+#' A collection of Planetary Systems table database column names
+#' and their respective labels / comments.
+#'
+#' @format A named list of 299 elements.
+#' @source \url{https://exoplanetarchive.ipac.caltech.edu/docs/API_PS_columns.html#columns}
+"ps_colnames"
+
+#' pscomppars_colnames
+#' A collection of Planetary Systems Composite Parameters table database column names
+#' and their respective labels / comments.
+#'
+#' @format A named list of 326 elements.
+#' @source \url{https://exoplanetarchive.ipac.caltech.edu/docs/API_PS_columns.html#columns}
+"pscomppars_colnames"
