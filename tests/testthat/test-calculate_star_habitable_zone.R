121c98a4571b23ddbc6db088eed339d8bc1d1f75
JKolomanski <kubula@onet.eu>
2025-08-08 16:55:45 +0200

    tests: improved test to use `expect_equal`

--- tests/testthat/test-calculate_star_habitable_zone.R
+++ tests/testthat/test-calculate_star_habitable_zone.R
@@ -4,7 +4,7 @@ describe("calculate_star_habitable_zone", {
 
     expected_st_lum = c(0.95, 1.37)
 
-    expect_true(identical(round(calculate_star_habitable_zone(test_st_lum), 2), expected_st_lum))
+    expect_equal(calculate_star_habitable_zone(test_st_lum), expected_st_lum, tolerance = 1e-2)
   })
 
   it("Calculates star's habitable zone with linear units", {
@@ -12,7 +12,7 @@ describe("calculate_star_habitable_zone", {
 
     expected_st_lum = c(0.95, 1.37)
 
-    expect_true(identical(round(calculate_star_habitable_zone(test_st_lum, log_lum = FALSE),
-                                2), expected_st_lum))
+    expect_equal(calculate_star_habitable_zone(test_st_lum, log_lum = FALSE),
+                 expected_st_lum, tolerance = 1e-2)
   })
 })
