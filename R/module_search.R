f902773052b1b10876e9d6d598088d3025860d11
m-kolomanski <m.kolomanski@yahoo.com>
2025-08-08 19:18:32 +0200

    feat: added randomization

--- R/module_search.R
+++ R/module_search.R
@@ -13,15 +13,24 @@ search_ui = function(id, label = "Search...", multiple = FALSE, random = FALSE) 
 
 search_server = function(id, choices) {
   shiny::moduleServer(id, function(input, output, session) {
-    logger::log_trace("{session$ns(id)} initialized.")
+    logger::log_trace("{id} initialized.")
 
     shiny::observe({
-      logger::log_trace("{session$ns(id)} updating choices.")
+      logger::log_trace("{id} updating choices.")
       shiny::updateSelectInput(
         inputId = "select",
         choices = c("", choices())
       )
     })
+
+    shiny::observe({
+      logger::log_trace("{id} randomizing choice.")
+      shiny::updateSelectInput(
+        inputId = "select",
+        selected = sample(choices(), 1)
+      )
+    }) |>
+      shiny::bindEvent(input$random)
 
     shiny::reactive(input$select)
   })
