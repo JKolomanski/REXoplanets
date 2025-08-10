d8cc9810691477f0583c3dc244986938eff7518f
m-kolomanski <m.kolomanski@yahoo.com>
2025-08-10 12:20:05 +0200

    tests: added tests for module_search

--- tests/testthat/test-e2e.R
+++ tests/testthat/test-e2e.R
@@ -1,3 +1,9 @@
+#' App driver from `shinytest2` has issues with stability when running from
+#' inside R CMD check - we are skipping the tests in that scenario.
+#' The tests will still run correctly when `devtools::test()` is called.
+if (!identical(Sys.getenv("_R_CHECK_PACKAGE_NAME_"), ""))
+  skip("Skipping app tests on R CMD check.")
+
 describe("app", {
   it("starts up the application correctly", {
     app = shinytest2::AppDriver$new(app(run = FALSE))
