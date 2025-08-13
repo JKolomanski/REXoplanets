9de760e3f1a12b88ce3cf8b23ce0f0bec6850636
JKolomanski <kubula@onet.eu>
2025-08-13 16:43:56 +0200

    docs: updated docs

--- R/module_visualize_star_system.R
+++ R/module_visualize_star_system.R
@@ -5,17 +5,26 @@
 #' for visualizing star systems based on data provided
 NULL
 
+#' @param id A unique identifier for the module.
+#' @returns A Shiny UI object.
+#' @describeIn module_visualize_star_system UI function for the system mapping module.
+#' @export
 visualize_star_system_ui = function(id) {
   ns = shiny::NS(id)
   shiny::plotOutput(ns("system_plot"))
 }
 
+#' @param id A unique identifier for the module.
+#' @param plot_data A reactive expression returning a data frame with information about the system.
+#' @returns A reactive expression containing the selected value(s).
+#' @describeIn module_visualize_star_system server function for the system mapping module.
+#' @export
 visualize_star_system_server = function(id, plot_data) {
   shiny::moduleServer(id, function(input, output, session) {
     logger::log_trace("{id} rendering star system plot")
 
     output$system_plot = shiny::renderPlot({
-      req(plot_data())
+      shiny::req(plot_data())
       plot_star_system(
         plot_data(),
         spectral_type = classify_star_spectral_type(plot_data()[["st_teff"]][1]),
